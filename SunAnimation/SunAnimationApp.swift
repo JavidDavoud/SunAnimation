9b216f609242e9879579274bb965e61f347699d6
Javid <Javid.d@780.team>
2024-01-15 16:37:52 +0330

    Initial Commit

---
+++ SunAnimation/SunAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SunAnimationApp.swift
+//  SunAnimation
+//
+//  Created by Javid on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SunAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
